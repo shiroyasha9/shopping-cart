a800be54d48397b78faec9b7edff0f3f2a652b54
Mubin Ansari <mubinansari910@gmail.com>
2023-09-10 19:52:08 +0530

    Add react native svg and transformer

--- App.tsx
+++ App.tsx
@@ -1,26 +1,14 @@
 import "react-native-gesture-handler";
 
 import { NavigationContainer } from "@react-navigation/native";
-import { SafeAreaView, StatusBar, useColorScheme } from "react-native";
-import { Colors } from "react-native/Libraries/NewAppScreen";
+import { StatusBar } from "react-native";
 import StackNavigator from "./src/navigation/StackNavigator";
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === "dark";
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
     <NavigationContainer>
-      <SafeAreaView style={[backgroundStyle, { flex: 1 }]}>
-        <StatusBar
-          barStyle={isDarkMode ? "light-content" : "dark-content"}
-          backgroundColor={backgroundStyle.backgroundColor}
-        />
-        <StackNavigator />
-      </SafeAreaView>
+      <StatusBar barStyle={"light-content"} backgroundColor={"transparent"} />
+      <StackNavigator />
     </NavigationContainer>
   );
 }

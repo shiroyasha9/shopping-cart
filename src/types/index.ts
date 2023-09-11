510e96f7304ac7c36da6281590ecf967be0db6fe
Mubin Ansari <mubinansari910@gmail.com>
2023-09-11 14:54:31 +0530

    Define type for Product API output

--- src/types/index.ts
+++ src/types/index.ts
@@ -51,3 +51,16 @@ export type OnboardingSlideData = {
   readonly subtitle: string;
   readonly Icon: React.ReactNode;
 };
+
+export type Product = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+};

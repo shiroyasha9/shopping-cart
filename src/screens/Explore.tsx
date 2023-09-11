e01edcfbe96dd7c79c975ac361e5b56d2b0ff576
Mubin Ansari <mubinansari910@gmail.com>
2023-09-11 14:54:57 +0530

    Implement basic UI for Explore

--- src/screens/Explore.tsx
+++ src/screens/Explore.tsx
@@ -1,7 +1,47 @@
-import { Text } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
+import { useQuery } from "react-query";
+import { InvisibleItem, ProductCard } from "../components";
+import { LoadingIndicator } from "../components/LoadingIndicator";
+import { Product } from "../types";
 
 const ExploreScreen = () => {
-  return <Text>Explore</Text>;
+  const { data, isLoading } = useQuery("products", async () => {
+    const response = await fetch("https://fakestoreapi.com/products");
+    if (!response.ok) {
+      throw new Error("Something went wrong");
+    }
+    return response.json() as Promise<Product[]>;
+  });
+
+  if (!data || isLoading) {
+    return <LoadingIndicator />;
+  }
+
+  return (
+    <View>
+      <FlatList
+        data={data}
+        style={styles.container}
+        renderItem={({ item, index }) => {
+          return (
+            <>
+              <ProductCard product={item} />
+              {index === data.length - 1 && index % 2 === 0 && (
+                <InvisibleItem />
+              )}
+            </>
+          );
+        }}
+        numColumns={2}
+      />
+    </View>
+  );
 };
 
 export default ExploreScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 20,
+  },
+});

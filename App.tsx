55f05b85882f11ec7b827371b87373809247dc75
Mubin Ansari <mubinansari910@gmail.com>
2023-09-11 14:54:20 +0530

    Setup react-query

--- App.tsx
+++ App.tsx
@@ -2,15 +2,20 @@ import "react-native-gesture-handler";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "react-native";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { PALETTE } from "./src/constants";
 import StackNavigator from "./src/navigation/StackNavigator";
 
+const queryClient = new QueryClient();
+
 function App(): JSX.Element {
   return (
-    <NavigationContainer>
-      <StatusBar barStyle="light-content" backgroundColor={PALETTE.orange} />
-      <StackNavigator />
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <StatusBar barStyle="light-content" backgroundColor={PALETTE.orange} />
+        <StackNavigator />
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
 

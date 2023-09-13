134fd93860a9254c122d8d6634ba333936c10003
Mubin Ansari <mubinansari910@gmail.com>
2023-09-13 13:42:31 +0530

    Take optional styles as props in LoadingIndicator

--- src/components/LoadingIndicator.tsx
+++ src/components/LoadingIndicator.tsx
@@ -1,22 +1,25 @@
-import { ActivityIndicator, View } from "react-native";
+import { ActivityIndicator, StyleSheet, View, ViewProps } from "react-native";
 import { PALETTE } from "../constants";
 
 type LoadingIndicatorProps = {
   size?: number | "small" | "large";
   color?: string;
+  style?: ViewProps["style"];
 };
 
 export const LoadingIndicator: React.FC<LoadingIndicatorProps> = (props) => {
-  const { size = "large", color = PALETTE.orange } = props;
+  const { size = "large", style, color = PALETTE.orange } = props;
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style={[styles.container, style]}>
       <ActivityIndicator size={size} color={color} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
